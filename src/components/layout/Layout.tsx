88e2eb240ac0c75a577eac30afdbab44a6655417
이진혁 <iih907@mediazen.co.kr>
2024-07-30 17:15:23 +0900

    기본 컴포넌트 작성 - Title, Button, Input, Header, Footer

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import Header from "../common/Header";
 import Footer from "../common/Footer";
+import { styled } from "styled-components";
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -9,10 +10,17 @@ function Layout({ children }: LayoutProps): JSX.Element {
     return (
         <>
             <Header />
-            <main>{children}</main>
+            <LayoutStyle>{children}</LayoutStyle>
             <Footer />
         </>
     )
 }
+
+const LayoutStyle = styled.div`
+    width: 100%;
+    margin: 0 auto;
+    max-width: ${({theme}) => theme.layout.width.large};
+    padding: 20px 0;
+`
 
 export default Layout;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,15 @@
+import Title from "../components/common/Title";
+import Button from "../components/common/Button";
+import InputText from "../components/common/InputText";
+
 function Home() {
     return (
         <>
+            {/*<Title size="medium" color="background">*/}
+            {/*    제목 테스트*/}
+            {/*</Title>*/}
+            {/*<Button size='large' scheme='normal'>버튼 테스트</Button>*/}
+            {/*<InputText placeholder='여기에 입력하세요'/>*/}
             <div>home body</div>
         </>
     )
