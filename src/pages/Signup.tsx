77e200cb75f08562680046b20735a00b5a6f7346
derulo taser <iih907@naver.com>
2024-10-27 01:25:06 +0900

    [feature] 회원가입 API fetch및 회원가입 화면 마무리 작업

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,16 +2,20 @@ import { styled } from "styled-components";
 import Title from "../components/common/Title";
 import InputText from "../components/common/InputText";
 import Button from "../components/common/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { signup } from "../api/auth.api";
+import { useAlert } from "../hooks/useAlert";
 
-interface SignupProps {
+export interface SignupProps {
     email: string;
     password: string;
 }
 
 function Signup() {
+    const navigate = useNavigate();
+    const showAlert = useAlert();
 //    const [email, setEmail] = useState("");
 //    const [password, setPassword] = useState("");
 
@@ -26,7 +30,10 @@ function Signup() {
     } = useForm<SignupProps>();
 
     const onSubmit = (data:SignupProps) => {
-        console.log(data);
+        signup(data).then((res) => {
+            showAlert('회원가입이 완료되었습니다.');
+            navigate('/login');
+        });
     }
 
 
