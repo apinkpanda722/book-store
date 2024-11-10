79a6e9993526d9344c74cddd918e3b01eb6a3232
derulo taser <iih907@naver.com>
2024-11-10 19:43:47 +0900

    [fix] useAuth hook 생성

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,12 +1,10 @@
 import Title from "../components/common/Title";
 import InputText from "../components/common/InputText";
 import Button from "../components/common/Button";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { resetPassword, resetRequest, signup } from "../api/auth.api";
-import { useAlert } from "../hooks/useAlert";
 import { SignupStyle } from "./Signup";
-import { useState } from "react";
+import { useAuth } from "@/hooks/useAuth";
 
 export interface SignupProps {
     email: string;
@@ -14,10 +12,7 @@ export interface SignupProps {
 }
 
 function ResetPassword() {
-    const navigate = useNavigate();
-    const { showAlert } = useAlert();
-    const [resetRequested, setResetRequested] = useState
-    (false);
+    const { userResetPassword, userResetRequest, resetRequested } = useAuth();
 
     const {
         register,
@@ -26,19 +21,8 @@ function ResetPassword() {
     } = useForm<SignupProps>();
 
     const onSubmit = (data:SignupProps) => {
-        if (resetRequested) {
-            resetPassword(data).then(() => {
-                showAlert("비밀번호가 초기화 되었습니다.");
-            });
-            navigate('/login');
-        } else {
-            resetRequest(data).then(() => {
-                setResetRequested(true);
-            });
-        }
+        resetRequested ? userResetPassword(data) : userResetRequest(data);
     }
-
-
 
     return (
         <>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,10 +2,8 @@ import { styled } from "styled-components";
 import Title from "../components/common/Title";
 import InputText from "../components/common/InputText";
 import Button from "../components/common/Button";
-import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { signup } from "../api/auth.api";
-import { useAlert } from "../hooks/useAlert";
+import { useAuth } from "@/hooks/useAuth";
 
 export interface SignupProps {
     email: string;
@@ -13,8 +11,7 @@ export interface SignupProps {
 }
 
 function Signup() {
-    const navigate = useNavigate();
-    const { showAlert } = useAlert();
+    const { userSignup } = useAuth();
 
     const {
         register,
@@ -22,14 +19,9 @@ function Signup() {
         formState : { errors }
     } = useForm<SignupProps>();
 
-    const onSubmit = (data:SignupProps) => {
-        signup(data).then((res) => {
-            showAlert('회원가입이 완료되었습니다.');
-            navigate('/login');
-        });
+    const onSubmit = (data: SignupProps) => {
+        userSignup(data);
     }
-
-
 
     return (
         <>
